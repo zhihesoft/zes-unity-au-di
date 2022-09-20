417f1b20aed2351c7bf9bd1ed9ba5584c01a6424
stanley <stanley@zhihesoft.com>
2022-09-20 13:49:46 +0800

    update to 0.1.2

---
+++ Packages/zes.au.di/Runtime/Scripts/InjectAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Au.DI
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public class InjectAttribute : Attribute
+    {
+        public InjectAttribute(string token)
+        {
+            this.token = token;
+        }
+
+        public InjectAttribute(int token)
+        {
+            this.token = token.ToString();
+        }
+
+        public readonly string token;
+    }
+}
